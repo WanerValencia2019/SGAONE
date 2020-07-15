b2544a22dd1534a2e9425233675c744c1a08c73f
INGE W. ANDRES VM <valenciawaner@gmail.com>
2020-07-15 14:18:13 -0500

    urls add login required

--- apps/gestor/urls.py
+++ apps/gestor/urls.py
@@ -1,21 +1,22 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     #path('',get_subjects,name="all")
-    path('teacher/home',teacher_home,name="teacher-home"),
-    path('teacher/account',teacher,name="teacher-account"),
-    path('teacher/courses',TeacherCourses.as_view(),name="teacher-courses"),
-    path('teacher/assignments',TeacherAssigment.as_view(),name="teacher-assigment"),
-    path('teacher/choices',createChoices,name="teacher-choices"),
-    path('teacher/assignments/create',TeacherAssigmentCreate.as_view(),name="teacher-assigment-create"),
-    path('teacher/assigments/edit/<int:pk>/',TeacherAssignmentUpdate.as_view(),name="teacher-assigment-update"),
-    path('teacher/assigments/delete/<int:pk>/',TeacherAssignmentDelete.as_view(),name="teacher-assigment-delete"),
-    path('teacher/gradedAssigment/',TeacherGradedAssignment.as_view(),name="teacher-graded-assigment"),
-    path('teacher/gradedAssigment/assignment/',getAssigmentCourse,name="graded-course-assigments"),
-    path('teacher/assignments/<title>/questions',QuestionAssignment.as_view(),name="detail-assigment-questions"),
-    path('teacher/courses/<course>',StudentCourse.as_view(),name="students-course"),
-    path('teacher/courses/<course>/<int:pk>',StudentDetailTeacher.as_view(),name="detail-student-teacher"),
+    path('teacher/home',login_required(teacher_home,login_url='login'),name="teacher-home"),
+    path('teacher/account',login_required(teacher,login_url='login'),name="teacher-account"),
+    path('teacher/courses',login_required(TeacherCourses.as_view(),login_url='login'),name="teacher-courses"),
+    path('teacher/assignments',login_required(TeacherAssigment.as_view(),login_url='login'),name="teacher-assigment"),
+    path('teacher/choices',login_required(createChoices,login_url='login'),name="teacher-choices"),
+    path('teacher/assignments/create',login_required(TeacherAssigmentCreate.as_view(),login_url='login'),name="teacher-assigment-create"),
+    path('teacher/assigments/edit/<int:pk>/',login_required(TeacherAssignmentUpdate.as_view(),login_url='login'),name="teacher-assigment-update"),
+    path('teacher/assigments/delete/<int:pk>/',login_required(TeacherAssignmentDelete.as_view(),login_url='login'),name="teacher-assigment-delete"),
+    path('teacher/gradedAssigment/',login_required(TeacherGradedAssignment.as_view(),login_url='login'),name="teacher-graded-assigment"),
+    path('teacher/gradedAssigment/assignment/',login_required(getAssigmentCourse,login_url='login'),name="graded-course-assigments"),
+    path('teacher/assignments/<title>/questions',login_required(QuestionAssignment.as_view(),login_url='login'),name="detail-assigment-questions"),
+    path('teacher/courses/<course>',login_required(StudentCourse.as_view(),login_url='login'),name="students-course"),
+    path('teacher/courses/<course>/<int:pk>',login_required(StudentDetailTeacher.as_view(),login_url='login'),name="detail-student-teacher"),
     
 ]
 
